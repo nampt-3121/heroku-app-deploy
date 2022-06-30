13e2ece86480ea4f67ca251c77549cd72e1dbcc0
pham.thanh.namb <SUN-ASTERISK\pham.thanh.namb@b123121-pc.sun-asterisk.com>
2022-06-30 10:22:13 +0700

    dsadsa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
     def index
-        render html: "<h1>Đừng hiều nhầm</h1>"
+        render html: "<h1>Đừng hiều nhầm</h1> màaaaa"
     end 
 end
